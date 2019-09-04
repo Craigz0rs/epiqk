7a135db15bd8741f688ad1ff3aea965a55ca3dfc
Craigz0rs <cdarcy@live.ca>
2019-09-04 13:28:18 -0400

    feat: 🎸 (Footer) init Footer component

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer class="site-footer" role="footer">
+        <div class="grid container">
+            
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+    name: "TheFooter"
+    
+}
+</script>
+<style lang="scss">
+
+</style>
